f06fe99ca2209f8dc4b35c6bc1f18e9f32446688
jakabij <jakabistvanj@gmail.com>
2020-07-22 11:23:42 +0200

    Style added for main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import PokemonList from "./components/PokemonList";
 import TypeList from "./components/TypeList";
@@ -12,12 +12,17 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div style={{ paddingLeft: 40 }}>
+        <div>
+          <img src="https://lh3.googleusercontent.com/proxy/cgWF6k0saMgPyUtQ5NPKnw_jpEbTNE5mlCOFcqufINa64m_hq1aVVeF2RsUFKYUbyw3c3oSf33qXNWXwctV2B5FsOyL8FPvH"/>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRmlwlZ6bkQBzmOFmQ8YR8pCf6EeYJaw6iZsw&usqp=CAU" style={{paddingLeft: "20%"}}/>
+        </div>
+        <nav style={{ paddingLeft: 40, border: '5px solid', borderColor: "cornflowerblue", backgroundColor: "yellow", textAlign: "center",
+    fontSize: "xxx-large", fontWeight: "900" }}>
           <a href="/Pokemons">Pokemons</a>
           <a style={{ paddingLeft: 40 }} href="/Types">
             Types
           </a>
-        </div>
+        </nav>
 
         <Switch>
           <Route path="/Pokemons">
