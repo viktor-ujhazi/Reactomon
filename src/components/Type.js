f93cd3941fecac0b04cee55be4abedef0687061c
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-07-22 11:10:25 +0200

    listing pokemon types

---
+++ src/components/Type.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class Type extends Component {
+//   componentDidMount = (props) => {
+//     console.log(this.props.pokemon);
+//   };
+
+  render() {
+    const { name } = this.props.type;
+    return (
+      <div>
+        <p>{name}</p>
+      </div>
+    );
+  }
+}
+// PropTypes
+Type.propTypes = {
+    pokemon: PropTypes.object.isRequired,
+  };
+
+export default Type;
--- src/components/TypeList.js
+++ src/components/TypeList.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import Type from './Type';
+import axios from 'axios';
 
 class TypeList extends Component {
+    state = {
+        types: [],
+      };
+    
+      componentDidMount() {
+        axios
+          .get("https://pokeapi.co/api/v2/type")
+          .then((res) => this.setState({ types: res.data.results }));
+      }
+    
+    
     render(){
-        return(
-        <div>
-            <a>Here we go with the types CUNT!</a>
-        </div>
-        );
+        // return(
+        // <div>
+        //     <a>Here we go with the types CUNT!</a>
+        // </div>
+        // );
+        return this.state.types.map((type) => (
+            <Type key={type.name} type={type} />
+          ));
+        
     }
 }
 
