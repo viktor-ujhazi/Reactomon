57e8ef6b83e7ab688c69b8ff44b0961ef4c1556d
jakabij <jakabistvanj@gmail.com>
2020-07-22 16:22:33 +0200

    design added for PokemonDetails

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
 import axios from "axios";
+import { Card } from "react-bootstrap";
 
 export class DetailPage extends Component {
   state = {
@@ -24,6 +25,7 @@ export class DetailPage extends Component {
             specialAttack: res.data.stats["3"].base_stat,
             specialDefense: res.data.stats["4"].base_stat,
             speed: res.data.stats["5"].base_stat,
+            img: res.data.sprites.front_default,
           },
         })
       );
@@ -31,13 +33,29 @@ export class DetailPage extends Component {
 
   render() {
     return (
-      <div>
-        {this.state.pokemon.name},{this.state.pokemon.base_experience},
+      <Card style={{width: "50%", height: "500px", textAlign: "center", display: "block",
+      marginLeft: "auto",marginRight: "auto", border:"solid", backgroundColor: "peachpuff",
+      marginBlockEnd: "30px", marginBlockStart: "30px"}}>
+          <Card.Body>
+            <Card.Title style={{fontSize: "xxx-large"}}>{this.state.pokemon.name}</Card.Title>
+            <Card.Img variant="top" src={this.state.pokemon.img} style={{height: "250px"}} />
+            <Card.Text>Base XP: {this.state.pokemon.base_experience}</Card.Text>
+            <Card.Text>Height: {this.state.pokemon.height}</Card.Text>
+            <Card.Text>HP: {this.state.pokemon.hp}</Card.Text>
+            <Card.Text>Attack: {this.state.pokemon.attack}</Card.Text>
+            <Card.Text>Defense: {this.state.pokemon.defense}</Card.Text>
+            <Card.Text>Special Attack: {this.state.pokemon.specialAttack}</Card.Text>
+            <Card.Text>Special Defense: {this.state.pokemon.specialDefense}</Card.Text>
+            <Card.Text>Speed: {this.state.pokemon.speed}</Card.Text>
+          </Card.Body>
+
+
+        {/* {this.state.pokemon.name},{this.state.pokemon.base_experience},
         {this.state.pokemon.height},{this.state.pokemon.hp},
         {this.state.pokemon.attack},{this.state.pokemon.defense},
         {this.state.pokemon.specialAttack},{this.state.pokemon.specialDefense},
-        {this.state.pokemon.speed}
-      </div>
+        {this.state.pokemon.speed} */}
+      </Card>
     );
   }
 }
