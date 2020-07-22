90acc18074d035c4995f09e70064fe88bc678b60
jakabij <jakabistvanj@gmail.com>
2020-07-22 14:47:41 +0200

    cards added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-
 import PokemonList from "./components/PokemonList";
 import TypeList from "./components/TypeList";
 import axios from "axios";
 import "./App.css";
+import {Navbar} from 'react-bootstrap';
 
 import "./App.css";
 
@@ -13,20 +13,20 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <img src="https://lh3.googleusercontent.com/proxy/cgWF6k0saMgPyUtQ5NPKnw_jpEbTNE5mlCOFcqufINa64m_hq1aVVeF2RsUFKYUbyw3c3oSf33qXNWXwctV2B5FsOyL8FPvH"/>
+          <img src="http://pixelartmaker.com/art/435fe298d486154.png"/>
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRmlwlZ6bkQBzmOFmQ8YR8pCf6EeYJaw6iZsw&usqp=CAU" style={{paddingLeft: "20%"}}/>
         </div>
-        <nav style={{ paddingLeft: 40, border: '5px solid', borderColor: "cornflowerblue", backgroundColor: "yellow", textAlign: "center",
-    fontSize: "xxx-large", fontWeight: "900" }}>
+        <Navbar style={{ paddingLeft: 40, border: '5px solid', borderColor: "cornflowerblue", backgroundColor: "yellow", textAlign: "center",
+          fontSize: "xxx-large", fontWeight: "900" }}>
           <a href="/Pokemons">Pokemons</a>
           <a style={{ paddingLeft: 40 }} href="/Types">
             Types
           </a>
-        </nav>
+        </Navbar>
 
         <Switch>
           <Route path="/Pokemons">
-            <PokemonList />
+            <ul><PokemonList /></ul>
           </Route>
 
           <Route path="/Types">
